72cff36216b91b921813195e74983ab62796ba7d
Hélio Márcio Filho <helio@inforio.com.br>
2017-06-27 18:10:20 -0300

    Criado demais Controllers

---
+++ src/main/java/com/github/heliomfdev/restful_ws/controller/EstadoController.java
@@ -0,0 +1,11 @@
+package com.github.heliomfdev.restful_ws.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/estado")
+public class EstadoController {
+
+
+}
--- src/main/java/com/github/heliomfdev/restful_ws/controller/PaisController.java
+++ src/main/java/com/github/heliomfdev/restful_ws/controller/PaisController.java
@@ -1,8 +1,10 @@
 package com.github.heliomfdev.restful_ws.controller;
 
+import com.github.heliomfdev.restful_ws.dto.PaisDTO;
 import com.github.heliomfdev.restful_ws.model.Estado;
 import com.github.heliomfdev.restful_ws.model.Pais;
 import com.github.heliomfdev.restful_ws.repository.PaisRepository;
+import com.github.heliomfdev.restful_ws.service.PaisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,15 +15,20 @@ import java.util.List;
 public class PaisController {
 
     @Autowired
-    PaisRepository paisRepository;
+    private PaisService service;
 
     @RequestMapping(value = "/sigla/{sigla}", method = RequestMethod.GET)
-    public Pais getBySigla(@PathVariable String sigla) {
-        return paisRepository.findBySigla(sigla);
+    public PaisDTO obterPorSigla(@PathVariable String sigla) {
+        return service.obterPorSigla(sigla);
     }
 
-    @RequestMapping(value = "/nome/{nome}")
-    public List<Pais> getByNome(@PathVariable String nome) {
-        return paisRepository.findByNomeContaining(nome);
+    @RequestMapping(value = "/nome/{nome}", method = RequestMethod.GET)
+    public List<PaisDTO> obterPorNome(@PathVariable String nome) {
+        return service.obterPorNome(nome);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<PaisDTO> obterTodos() {
+        return service.obterTodos();
     }
 }
